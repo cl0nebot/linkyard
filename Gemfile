8e520d9300689c9a5d88f765654696a08a33ebdf
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-05-19 11:38:40 +1200

    Twitter interaction @wip

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'byebug'
 gem 'devise'
 gem 'pure-css-rails'
 gem 'sidekiq'
+gem 'twitter'
+gem 'omniauth-twitter'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/twitter_interaction.rb
+++ app/models/twitter_interaction.rb
@@ -1,4 +1,19 @@
 class TwitterInteraction < Interaction
-  validates :consumer_key, :consumer_secret, :access_token, :access_token_secret, :presence => true
-  store_accessor :configuration, :consumer_key, :consumer_secret, :access_token, :access_token_secret
+  validates :access_token, :access_token_secret, :presence => true
+  store_accessor :configuration, :access_token, :access_token_secret
+
+  def act(link)
+    client = create_twitter_client
+    client.update("Tweet by Twitter gem")
+  end
+
+  protected
+  def create_twitter_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.secrets.twitter_api_key
+      config.consumer_secret = Rails.application.secrets.twitter_api_secret
+      config.access_token = access_token
+      config.access_token_secret = access_token_secret
+    end
+  end
 end
