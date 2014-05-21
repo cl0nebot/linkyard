7aa907a440514f7ee1116e29952a14ee988b341d
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-05-21 16:39:58 +1200

    Remove unwanted functionality

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,114 +1,26 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
-  require 'uuidtools'
-
-  def facebook
-    oauthorize "Facebook"
-  end
-
   def twitter
-    oauthorize "Twitter"
+    access_token = env["omniauth.auth"]
+
+    uid = access_token['extra']['raw_info']['id']
+    name = access_token['extra']['raw_info']['name']
+    authorization_attributes = { 
+      :uid => uid, 
+      :token => access_token['credentials']['token'],
+      :secret => access_token['credentials']['secret'], 
+      :first_name => access_token['info']['first_name'],
+      :last_name => access_token['info']['last_name'], 
+      :name => name,
+      :link => "http://twitter.com/#{name}",
+      :provider => "Twitter"
+    }
+
+    unless current_user.authorizations.exists?(:provider => "Twitter")
+      current_user.authorizations.build(authorization_attributes)
+      current_user.save!
+      session["devise.twitter_data"] = access_token
+    end
+
     redirect_to edit_user_registration_path 
   end
-
-  def linkedin
-    oauthorize "LinkedIn"
-  end
-
-  def passthru
-    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
-  end
-
-private
-
-  def oauthorize(kind)
-    @user = find_for_ouath(kind, env["omniauth.auth"], current_user)
-    if @user && !user_signed_in?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => kind
-      session["devise.#{kind.downcase}_data"] = env["omniauth.auth"]
-      sign_in_and_redirect @user, :event => :authentication     
-    end
-  end
-
-  def find_for_ouath(provider, access_token, resource=nil)
-    user, email, name, uid, auth_attr = nil, nil, nil, {}
-    case provider
-      when "Facebook"
-        uid = access_token['uid']
-        email = access_token['info']['email']
-        auth_attr = { :uid => uid, :token => access_token['credentials']['token'],
-          :secret => nil, :first_name => access_token['info']['first_name'],
-          :last_name => access_token['info']['last_name'], :name => access_token['info']['name'],
-          :link => access_token['extra']['raw_info']['link'] }
-      when "Twitter"
-        uid = access_token['extra']['raw_info']['id']
-        name = access_token['extra']['raw_info']['name']
-        auth_attr = { :uid => uid, :token => access_token['credentials']['token'],
-          :secret => access_token['credentials']['secret'], :first_name => access_token['info']['first_name'],
-          :last_name => access_token['info']['last_name'], :name => name,
-          :link => "http://twitter.com/#{name}" }
-      when 'LinkedIn'
-        uid = access_token['uid']
-        name = access_token['info']['name']
-        auth_attr = { :uid => uid, :token => access_token['credentials']['token'],
-          :secret => access_token['credentials']['secret'], :first_name => access_token['info']['first_name'],
-          :last_name => access_token['info']['last_name'],
-          :link => access_token['info']['public_profile_url'] }
-    else
-      raise 'Provider #{provider} not handled'
-    end
-
-    if resource.nil?
-      if email
-        user = find_for_oauth_by_email(email, resource)
-      elsif uid && name
-        user = find_for_oauth_by_uid(uid, resource)
-        if user.nil?
-          user = find_for_oauth_by_name(name, resource)
-        end
-      end
-    else
-      user = resource
-    end
-
-    auth = user.authorizations.find_by_provider(provider)
-    if auth.nil?
-      auth = user.authorizations.build(:provider => provider)
-      user.authorizations << auth
-    end
-    auth.update_attributes auth_attr
-
-    return user
-  end
-
-  def find_for_oauth_by_uid(uid, resource=nil)
-    user = nil
-    if auth = Authorization.find_by_uid(uid.to_s)
-      user = auth.user
-    end
-    return user
-  end
-
-  def find_for_oauth_by_email(email, resource=nil)
-    if user = User.find_by_email(email)
-      user
-    else
-      user = User.new(:email => email, :password => Devise.friendly_token[0,20])
-      user.save
-    end
-    return user
-  end
-
-  def find_for_oauth_by_name(name, resource=nil)
-    if user = User.find_by_name(name)
-      user
-    else
-      first_name = name
-      last_name = name
-      user = User.new(:first_name => first_name, :last_name => last_name, :password => Devise.friendly_token[0,20], :email => "#{UUIDTools::UUID.random_create}@host")
-      user.save(:validate => false)
-    end
-    return user
-  end
-
 end
