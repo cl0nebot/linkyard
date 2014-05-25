23208c8f0db5569e02f2699f9461b5ba6014228c
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-05-25 20:04:00 +1200

    Interaction specs

--- spec/models/interaction_spec.rb
+++ spec/models/interaction_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe Interaction do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe ".new_by_type" do
+
+    def new_by_type(type)
+      Interaction.new_by_type(type)
+    end 
+     
+    context "when creating twitter interaction" do
+      subject { new_by_type("TwitterInteraction") }
+      it { should be_instance_of TwitterInteraction }
+    end
+  
+    context "when creating reddit interaction" do     
+      subject { new_by_type("RedditInteraction") }
+      it { should be_instance_of RedditInteraction }
+    end
+
+    it "when creating invalid interaction" do
+      expect { new_by_type("NotSupportedInteraction") }.to raise_error
+    end
+  end
+  
+  describe ".humanize_type" do
+    def humanize_type(type)
+      Interaction.humanize_type(type)
+    end
+
+    context "when twitter interaction is passed" do
+      subject { humanize_type("TwitterInteraction") }
+      it { should match "Twitter"}
+    end
+
+    context "when reddit interaction is passed" do
+      subject { humanize_type("RedditInteraction") }
+      it { should match "Reddit" }
+    end
+  end
+
+  describe "#act" do
+    it "raises as it is an abstract method" do
+      expect { Interaction.new.act }.to raise_error
+    end
+  end
+
 end
