7d20fb29084a7f241cda2822d161eb3e679d361b
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-05-26 09:39:39 +1200

    Interaction #name tested

--- spec/models/interaction_spec.rb
+++ spec/models/interaction_spec.rb
@@ -1,20 +1,24 @@
 require 'spec_helper'
 
 describe Interaction do
-  
-  describe ".new_by_type" do
+  let(:twitter_interaction_name) { "Twitter" }
+  let(:reddit_interaction_name) { "Reddit" }
+  let(:twitter_interaction_type) { "TwitterInteraction" }
+  let(:reddit_interaction_type) { "RedditInteraction" }
 
-    def new_by_type(type)
-      Interaction.new_by_type(type)
-    end 
+  def new_by_type(type)
+    Interaction.new_by_type(type)
+  end 
+
+  describe ".new_by_type" do
      
     context "when creating twitter interaction" do
-      subject { new_by_type("TwitterInteraction") }
+      subject { new_by_type(twitter_interaction_type) }
       it { should be_instance_of TwitterInteraction }
     end
   
     context "when creating reddit interaction" do     
-      subject { new_by_type("RedditInteraction") }
+      subject { new_by_type(reddit_interaction_type) }
       it { should be_instance_of RedditInteraction }
     end
 
@@ -29,19 +33,26 @@ describe Interaction do
     end
 
     context "when twitter interaction is passed" do
-      subject { humanize_type("TwitterInteraction") }
-      it { should match "Twitter"}
+      subject { humanize_type(twitter_interaction_type) }
+      it { should match twitter_interaction_name }
     end
 
     context "when reddit interaction is passed" do
-      subject { humanize_type("RedditInteraction") }
-      it { should match "Reddit" }
+      subject { humanize_type(reddit_interaction_type) }
+      it { should match reddit_interaction_name }
     end
   end
 
   describe "#act" do
     it "raises as it is an abstract method" do
       expect { Interaction.new.act }.to raise_error
+    end
+  end
+
+  describe "#name" do
+    it "should return reasonable name for existing interactions" do
+      new_by_type(twitter_interaction_type).name.should match twitter_interaction_name
+      new_by_type(reddit_interaction_type).name.should match reddit_interaction_name
     end
   end
 
