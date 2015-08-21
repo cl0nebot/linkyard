c1dd205d852338f6697b57c8e66dd72ac87807a5
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-08-21 21:06:18 +1200

    Fix subscriber can be subscribed to mltiple digests

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,7 +1,7 @@
 class Subscriber < ActiveRecord::Base
   include ActiveUUID::UUID
 
-  validates :email,:email_format => { message: 'is not a valid email address' }, uniqueness: { case_sensitive: false, message: 'is already subscribed' }
+  validates :email,:email_format => { message: 'is not a valid email address' }, uniqueness: { scope: :digest, case_sensitive: false, message: 'is already subscribed' }
   validates :digest, length: { maximum: 20 }, presence: true
 
   scope :active, -> { where unsubscribed_at: nil }
