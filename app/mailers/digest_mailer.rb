4982f77e048182cf9f2ba65675a13b1f8509f630
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-08-24 07:29:00 +1200

    Don't forget to commit the other part of the commit errr .

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -2,16 +2,14 @@ class DigestMailer < ActionMailer::Base
   include ApplicationHelper
   add_template_helper(ApplicationHelper)
 
-  def weekly(type)
-    @digest = Weekly::Digest.current_digest(type)
+  def weekly(digest, subscriber)
+    @digest = digest
+    @subscriber = subscriber
     headers['X-MC-PreserveRecipients'] = false
-    Subscriber.active.where(digest: type).each do |subscriber|
-      @subscriber = subscriber
-      mail to: @subscriber.email,
-           from: "jakub@#{@digest.type}digest.net",
-           reply_to: "jakub.chodounsky@gmail.com",
-           subject: "Weekly #{digest_name(@digest.type)} Digest ##{@digest.issue}",
-           delivery_method_options: { domain: @digest.domain }
-    end
+    mail to: @subscriber.email,
+      from: "jakub@#{@digest.type}digest.net",
+      reply_to: "jakub.chodounsky@gmail.com",
+      subject: "Weekly #{digest_name(@digest.type)} Digest ##{@digest.issue}",
+      delivery_method_options: { domain: @digest.domain }
   end
 end
