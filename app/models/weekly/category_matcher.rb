104db5801ec4bd87fd4c56647cca70ee21f56b20
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-09-16 07:47:32 +1200

    Add sponsor to the front as ordering matters

--- app/models/weekly/category_matcher.rb
+++ app/models/weekly/category_matcher.rb
@@ -5,35 +5,35 @@ module Weekly
       Weekly::Digest::PROGRAMMING => {
         default: Category.new("general", 1, []),
         categories: [
+          Category.new("sponsor", 0, ["sponsor"]),
           Category.new("ruby", 2, ["ruby", "rails"]),
           Category.new("webdev", 3, ["html", "javascript", "webdev", "css"]),
           Category.new("fun", 5, ["programmingfun", "funny"]),
-          Category.new("sponsor", 0, ["sponsor"])
         ]
       },
       Weekly::Digest::PHOTOGRAPHY => {
         default: Category.new("general", 1, []),
         categories: [
+          Category.new("sponsor", 0, ["sponsor"]),
           Category.new("photos of the week", 2, ["photo"]),
           Category.new("tutorials", 3, ["tutorial"]),
           Category.new("gear reviews", 4, ["gear"]),
-          Category.new("sponsor", 0, ["sponsor"])
         ]
       },
       Weekly::Digest::CSHARP => {
         default: Category.new(".net", 1, []),
         categories: [
+          Category.new("sponsor", 0, ["sponsor"]),
           Category.new("asp.net", 2, ["aspnet", "iis"]),
           Category.new("azure", 3, ["azure"]),
           Category.new("wpf", 4, ["wpf"]),
-          Category.new("sponsor", 0, ["sponsor"])
         ]
       },
       Weekly::Digest::ELIXIR => {
         default: Category.new("elixir", 1, []),
         categories: [
+          Category.new("sponsor", 0, ["sponsor"]),
           Category.new("phoenix", 2, ["phoenix"]),
-          Category.new("sponsor", 0, ["sponsor"])
         ]
       },
       Weekly::Digest::REACT => {
