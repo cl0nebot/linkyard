7b799127ead08aa65eb9725472a0a953da91a1ee
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-11-04 08:47:48 +1300

    Add view in browser, social share and other digests to email vie

--- spec/mailers/previews/digest_mailer_preview.rb
+++ spec/mailers/previews/digest_mailer_preview.rb
@@ -1,6 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/digest_mailer
 class DigestMailerPreview < ActionMailer::Preview
   def weekly
-    DigestMailer.weekly(Weekly::Digest::PROGRAMMING)
+    type = Weekly::Digest::PROGRAMMING
+    DigestMailer.weekly(Weekly::Digest.new(type, issue: 119), Subscriber.for_email.where(digest: type).last)
   end
 end
