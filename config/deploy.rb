04efcc6c989a1653abf03b756870eb41d97dd14b
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-11-16 08:17:13 +1300

    Remove bin from capistrano linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:jakubgarfield/linkyard.git'
 set :deploy_to, '/home/deploy/linkyard'
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 namespace :deploy do
