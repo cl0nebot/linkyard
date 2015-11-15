de5f6e3eed1c33b0e978ea488c293a5cacb5b613
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-11-16 09:17:20 +1300

    Enable OSSP to make UIID work

--- db/schema.rb
+++ db/schema.rb
@@ -11,53 +11,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012193830) do
+ActiveRecord::Schema.define(version: 20151115201630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
-  create_table "authorizations", force: true do |t|
-    t.string   "provider"
-    t.string   "uid"
+  create_table "authorizations", force: :cascade do |t|
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
     t.integer  "user_id"
-    t.string   "token"
-    t.string   "secret"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "name"
-    t.string   "link"
+    t.string   "token",      limit: 255
+    t.string   "secret",     limit: 255
+    t.string   "first_name", limit: 255
+    t.string   "last_name",  limit: 255
+    t.string   "name",       limit: 255
+    t.string   "link",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "digest"
+    t.string   "digest",     limit: 255
   end
 
-  create_table "best_times", force: true do |t|
-    t.string   "interaction"
+  create_table "best_times", force: :cascade do |t|
+    t.string   "interaction", limit: 255
     t.integer  "time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "interactions", force: true do |t|
-    t.integer  "user_id",           null: false
-    t.string   "type",              null: false
+  create_table "interactions", force: :cascade do |t|
+    t.integer  "user_id",                       null: false
+    t.string   "type",              limit: 255, null: false
     t.text     "configuration"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "post_at_best_time"
   end
 
-  create_table "link_interactions", force: true do |t|
-    t.integer  "interaction_id",      null: false
-    t.integer  "link_id",             null: false
+  create_table "link_interactions", force: :cascade do |t|
+    t.integer  "interaction_id",                  null: false
+    t.integer  "link_id",                         null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "status"
-    t.string   "status_description"
+    t.string   "status",              limit: 255
+    t.string   "status_description",  limit: 255
     t.datetime "best_scheduled_time"
   end
 
-  create_table "link_tags", force: true do |t|
+  create_table "link_tags", force: :cascade do |t|
     t.integer  "link_id",                    null: false
     t.integer  "tag_id",                     null: false
     t.datetime "created_at"
@@ -65,28 +66,27 @@ ActiveRecord::Schema.define(version: 20151012193830) do
     t.boolean  "default",    default: false
   end
 
-  create_table "links", force: true do |t|
-    t.integer  "user_id",     null: false
-    t.string   "title",       null: false
-    t.string   "url",         null: false
+  create_table "links", force: :cascade do |t|
+    t.integer  "user_id",                 null: false
+    t.string   "title",       limit: 255, null: false
+    t.string   "url",         limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
     t.text     "content"
-    t.string   "digest"
+    t.string   "digest",      limit: 255
   end
 
-  create_table "pg_search_documents", force: true do |t|
+  create_table "pg_search_documents", force: :cascade do |t|
     t.text     "content"
     t.integer  "searchable_id"
-    t.string   "searchable_type"
+    t.string   "searchable_type", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "subscribers", id: false, force: true do |t|
-    t.uuid     "id",                         null: false
-    t.string   "email"
+  create_table "subscribers", id: :uuid, default: nil, force: :cascade do |t|
+    t.string   "email",           limit: 255
     t.string   "digest",          limit: 20
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -96,27 +96,27 @@ ActiveRecord::Schema.define(version: 20151012193830) do
 
   add_index "subscribers", ["id"], name: "index_subscribers_on_id", using: :btree
 
-  create_table "tags", force: true do |t|
-    t.integer  "user_id",    null: false
-    t.string   "name",       null: false
+  create_table "tags", force: :cascade do |t|
+    t.integer  "user_id",                null: false
+    t.string   "name",       limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "authentication_token"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "authentication_token",   limit: 255
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
