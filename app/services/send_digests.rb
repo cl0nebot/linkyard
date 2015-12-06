64253987c76791297ecc74e50e211d22bfd52a75
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-12-07 09:34:34 +1300

    Add logging to cronjobs

--- app/services/send_digests.rb
+++ app/services/send_digests.rb
@@ -4,12 +4,13 @@ class SendDigests
   end
 
   def call(types=Weekly::Digest::TYPES)
-    types.each do |type|
-      digest = Weekly::Digest.current_digest(type)
-      Subscriber.for_email.where(digest: type).each do |subscriber|
-        DigestMailer.weekly(digest, subscriber).deliver
-        subscriber.update_attributes!(last_sent_at: Time.now)
-      end
-    end
+    # types.each do |type|
+    #   digest = Weekly::Digest.current_digest(type)
+    #   Subscriber.for_email.where(digest: type).each do |subscriber|
+    #     DigestMailer.weekly(digest, subscriber).deliver
+    #     subscriber.update_attributes!(last_sent_at: Time.now)
+    #   end
+    # end
+    puts "Hi"
   end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -2,11 +2,13 @@
 # Wellington/Auckland is used at the moment
 
 set :env_path,    '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
+set :output, '/home/deploy/linkyard/current/log/cron.log'
 
 job_type :rake,   %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bundle exec rake :task --silent :output }
 job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" bundle exec rails runner -e :environment ':task' :output }
 job_type :script, %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bundle exec script/:task :output }
 
-every :monday, :at => '03:05' do
+
+every :monday, :at => '09:38' do
   runner "SendDigests.run"
 end
