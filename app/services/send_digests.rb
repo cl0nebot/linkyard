d67ac8bb6309f903a565c28b21dfdc0263353614
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-12-09 07:56:04 +1300

    Add logging for cron task

--- app/services/send_digests.rb
+++ app/services/send_digests.rb
@@ -5,11 +5,14 @@ class SendDigests
 
   def call(types=Weekly::Digest::TYPES)
     types.each do |type|
-      digest = Weekly::Digest.current_digest(type)
-      Subscriber.for_email.where(digest: type).each do |subscriber|
-        DigestMailer.weekly(digest, subscriber).deliver
-        subscriber.update_attributes!(last_sent_at: Time.now)
-      end
+      puts "#{Time.zone.now} - Starting to send newsletters for #{type}."
+      # digest = Weekly::Digest.current_digest(type)
+      # Subscriber.for_email.where(digest: type).each do |subscriber|
+      #   DigestMailer.weekly(digest, subscriber).deliver
+      #   subscriber.update_attributes!(last_sent_at: Time.now)
+      # end
+      puts "#{Time.zone.now} - Newsletters sent successfully for #{type}."
     end
+    puts "#{Time.zone.now} - All newsletters sent successfully."
   end
 end
