3ba9d4d502506b1b9061f7b54f67860da644448d
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-03-02 14:53:13 +1300

    Convert spam score to number

--- app/controllers/inbound_emails_controller.rb
+++ app/controllers/inbound_emails_controller.rb
@@ -27,7 +27,7 @@ class InboundEmailsController < ApplicationController
     from = params[:from]
     subject = params[:subject]
     text = params[:html] || params[:text]
-    spam_score = params[:spam_score]
+    spam_score = params[:spam_score].to_f
 
     InboundMailer.inbound(to, from, subject, text).deliver unless spam_score > 5
   end
