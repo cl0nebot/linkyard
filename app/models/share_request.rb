8fdfd3586838da2b7cc5ab8ab1b66a3519ff05a3
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-05-20 20:44:42 +1200

    Nicer way of making it shorter

--- app/models/share_request.rb
+++ app/models/share_request.rb
@@ -4,7 +4,7 @@ class ShareRequest < ActiveRecord::Base
   belongs_to :link
 
   def tweet_text
-    "@#{twitter_contact} \"#{link.title.slice(0..79}\" was featured in last digest. Share it #{tweet_url}"
+    "@#{twitter_contact} \"#{title(link)}\" was featured in last digest. Share it #{tweet_url}"
   end
 
   def tweet_url
@@ -12,5 +12,14 @@ class ShareRequest < ActiveRecord::Base
     issue = Weekly::Digest.issue_from(link.digest, link.created_at)
     url = Rails.application.routes.url_helpers.digest_url(issue, host: Weekly::Digest.domain_from(link.digest))
     "https://twitter.com/intent/tweet?text=#{text}&url=#{url}&via=#{twitter_username(link.digest)}"
+  end
+
+  private
+  def title(link)
+    if link.title.length > 79
+      link.title.slice(0..76) + "..."
+    else
+      link.title
+    end
   end
 end
