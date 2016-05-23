ac66ab597d2cefad63c5281c7383028c011b959e
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-05-24 07:36:42 +1200

    Change the share request text a little

--- app/models/share_request.rb
+++ app/models/share_request.rb
@@ -4,22 +4,25 @@ class ShareRequest < ActiveRecord::Base
   belongs_to :link
 
   def tweet_text
-    "@#{twitter_contact} \"#{title(link)}\" was featured in last digest. Share it #{tweet_url}"
+    issue = Weekly::Digest.issue_from(link.digest, link.created_at)
+    url = Rails.application.routes.url_helpers.digest_url(issue, host: Weekly::Digest.domain_from(link.digest))
+    ".@#{twitter_contact} \"#{title(link)}\" was featured in last digest at #{url}"
   end
 
+  private
+  def title(link)
+    if link.title.length > 75
+      link.title.slice(0..72) + "..."
+    else
+      link.title
+    end
+  end
+
+  # not used at the moment
   def tweet_url
     text = CGI.escape("#{link.title.strip} featured in #{link.digest.capitalize} Digest at")
     issue = Weekly::Digest.issue_from(link.digest, link.created_at)
     url = Rails.application.routes.url_helpers.digest_url(issue, host: Weekly::Digest.domain_from(link.digest))
     "https://twitter.com/intent/tweet?text=#{text}&url=#{url}&via=#{twitter_username(link.digest)}"
-  end
-
-  private
-  def title(link)
-    if link.title.length > 79
-      link.title.slice(0..76) + "..."
-    else
-      link.title
-    end
   end
 end
