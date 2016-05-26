dfacbb425c1891f13be61ba0fddb9cdb64b4da06
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-05-27 08:06:22 +1200

    Add correct version of digest for links

--- app/models/share_request.rb
+++ app/models/share_request.rb
@@ -21,7 +21,7 @@ class ShareRequest < ActiveRecord::Base
   # not used at the moment
   def tweet_url
     text = CGI.escape("#{link.title.strip} featured in #{link.digest.capitalize} Digest at")
-    issue = Weekly::Digest.issue_from(link.digest, link.created_at)
+    issue = Weekly::Digest.issue_from(link.digest, link.created_at) + 1
     url = Rails.application.routes.url_helpers.digest_url(issue, host: Weekly::Digest.domain_from(link.digest))
     "https://twitter.com/intent/tweet?text=#{text}&url=#{url}&via=#{twitter_username(link.digest)}"
   end
