19d3beabb02bdc8fa869161d098d7416f145ebc9
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-08-07 09:41:48 +1200

    End date categories and change email perex

--- app/models/weekly/category.rb
+++ app/models/weekly/category.rb
@@ -1,11 +1,12 @@
 module Weekly
   class Category
-    attr_reader :name, :order
+    attr_reader :name, :order, :end_date
 
-    def initialize(name, order, tags)
+    def initialize(name, order, tags, end_date: nil)
       @name = name
       @order = order
       @tags = tags
+      @end_date = end_date
     end
 
     def include?(link_tags)
--- app/models/weekly/category_matcher.rb
+++ app/models/weekly/category_matcher.rb
@@ -3,12 +3,12 @@ module Weekly
 
     CONFIGURATION = {
       Weekly::Digest::PROGRAMMING => {
-        default: Category.new("general", 1, []),
+        default: Category.new("programming", 1, []),
         categories: [
           Category.new("sponsor", 0, ["sponsor"]),
-          Category.new("ruby", 2, ["ruby", "rails"]),
-          Category.new("webdev", 3, ["html", "javascript", "webdev", "css"]),
-          Category.new("fun", 5, ["programmingfun", "funny", "fun"]),
+          Category.new("ruby", 2, ["ruby", "rails"], end_date: Date.parse('2016-07-31')),
+          Category.new("webdev", 3, ["html", "javascript", "webdev", "css"], end_date: Date.parse('2016-07-31')),
+          Category.new("fun", 5, ["programmingfun", "funny", "fun"], end_date: Date.parse('2016-07-31')),
           Category.new("jobs", 100, ["job", "jobs"]),
         ]
       },
@@ -16,9 +16,9 @@ module Weekly
         default: Category.new(".net", 1, []),
         categories: [
           Category.new("sponsor", 0, ["sponsor"]),
-          Category.new("asp.net", 2, ["aspnet", "iis"]),
-          Category.new("azure", 3, ["azure"]),
-          Category.new("wpf", 4, ["wpf"]),
+          Category.new("asp.net", 2, ["aspnet", "iis"], end_date: Date.parse('2016-07-31')),
+          Category.new("azure", 3, ["azure"], end_date: Date.parse('2016-07-31')),
+          Category.new("wpf", 4, ["wpf", end_date: Date.parse('2016-07-31')]),
           Category.new("jobs", 100, ["job", "jobs"]),
         ]
       },
@@ -26,7 +26,7 @@ module Weekly
         default: Category.new("elixir", 1, []),
         categories: [
           Category.new("sponsor", 0, ["sponsor"]),
-          Category.new("phoenix", 2, ["phoenix"]),
+          Category.new("phoenix", 2, ["phoenix"], end_date: Date.parse('2016-07-31')),
           Category.new("jobs", 100, ["job", "jobs"]),
         ]
       },
@@ -34,14 +34,14 @@ module Weekly
         default: Category.new("react", 1, []),
         categories: [
           Category.new("sponsor", 0, ["sponsor"]),
-          Category.new("react native", 2, ["reactnative"])
+          Category.new("react native", 2, ["reactnative"], end_date: Date.parse('2016-07-31')),
           Category.new("jobs", 100, ["job", "jobs"]),
         ]
       }
     }
 
     def self.match(type, link)
-      CONFIGURATION[type][:categories].find { |c| c.include?(link.tags.map { |t| t.name }) } || CONFIGURATION[type][:default]
+      CONFIGURATION[type][:categories].find { |c| c.include?(link.tags.map { |t| t.name }) && (c.end_date.nil? || link.created_at <= c.end_date) } || CONFIGURATION[type][:default]
     end
   end
 end
