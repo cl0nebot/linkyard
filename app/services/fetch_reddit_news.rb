84a89af13f4cf7b6569627e3ac9eb9a21c7e2292
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-09-15 13:12:26 +1200

    Fix nil when no articles

--- app/services/fetch_reddit_news.rb
+++ app/services/fetch_reddit_news.rb
@@ -7,7 +7,7 @@ class FetchRedditNews
   end
 
   def call
-    links = RedditKit.links(@subreddit, category: :top, time: @period, limit: 10)
+    links = RedditKit.links(@subreddit, category: :top, time: @period, limit: 10) || []
     links.map { |l| RedditLink.new(l.url, l.title, l.upvotes, l.created_at) }
   end
 end
