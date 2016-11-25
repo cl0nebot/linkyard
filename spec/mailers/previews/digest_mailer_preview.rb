524e406950a5fe59f26b249a800742b3b0e59ee1
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-11-25 14:38:24 +1300

    More https

--- spec/mailers/previews/digest_mailer_preview.rb
+++ spec/mailers/previews/digest_mailer_preview.rb
@@ -2,15 +2,15 @@
 class DigestMailerPreview < ActionMailer::Preview
   def weekly
     type = Weekly::Digest::PROGRAMMING
-    return DigestMailer.weekly(Weekly::Digest.new(type, issue: 175), Subscriber.for_email.where(digest: type).last)
+    return DigestMailer.weekly(Weekly::Digest.new(type, issue: 175), Subscriber.where(digest: type).last)
 
     # type = Weekly::Digest::CSHARP
-    # return DigestMailer.weekly(Weekly::Digest.new(type, issue: 107), Subscriber.for_email.where(digest: type).last)
+    # return DigestMailer.weekly(Weekly::Digest.new(type, issue: 107), Subscriber.where(digest: type).last)
 
     # type = Weekly::Digest::ELIXIR
-    # return DigestMailer.weekly(Weekly::Digest.new(type, issue: 50), Subscriber.for_email.where(digest: type).last)
+    # return DigestMailer.weekly(Weekly::Digest.new(type, issue: 50), Subscriber.where(digest: type).last)
 
     # type = Weekly::Digest::REACT
-    # return DigestMailer.weekly(Weekly::Digest.new(type, issue: 46), Subscriber.for_email.where(digest: type).last)
+    # return DigestMailer.weekly(Weekly::Digest.new(type, issue: 46), Subscriber.where(digest: type).last)
   end
 end
